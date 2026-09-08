f64cae2ef7b1c4bbd42727a25778059303038276
Tayfun <tayfuna766@gmail.com>
2026-09-08 23:12:27 +0300

    feat(r7): load full admin design layer

--- views/admin/layout.php
+++ views/admin/layout.php
@@ -31,8 +31,9 @@ $_user   = $_user ?? null;
 <link rel="icon" href="/assets/img/favicon-32.png" sizes="32x32">
 <link rel="stylesheet" href="<?= Security::e(asset('css/admin.css')) ?>">
 <link rel="stylesheet" href="<?= Security::e(asset('css/admin-redesign.css')) ?>">
+<link rel="stylesheet" href="<?= Security::e(asset('css/admin-r7.css')) ?>">
 </head>
-<body class="admin">
+<body class="admin admin--r7">
 
 <a class="skip-link" href="#panel-icerik">İçeriğe geç</a>
 
