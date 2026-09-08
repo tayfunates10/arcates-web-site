4b4b6d9dd1fe016dd0f755cfad8613507f9b0929
Tayfun <tayfuna766@gmail.com>
2026-09-08 22:20:39 +0300

    perf(r6): load sticky performance guard

--- views/front/partials/head.php
+++ views/front/partials/head.php
@@ -62,6 +62,7 @@ if ($ogImage === '') {
 <link rel="stylesheet" href="<?= Security::e(asset('css/site.css')) ?>">
 <link rel="stylesheet" href="<?= Security::e(asset('css/redesign.css')) ?>">
 <link rel="stylesheet" href="<?= Security::e(asset('css/inner-redesign.css')) ?>">
+<link rel="stylesheet" href="<?= Security::e(asset('css/inner-performance.css')) ?>">
 <?php foreach ((array) ($head['styles'] ?? []) as $style): ?>
 <link rel="stylesheet" href="<?= Security::e(asset((string) $style)) ?>">
 <?php endforeach; ?>
