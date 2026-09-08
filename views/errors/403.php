2385cc9a4d3bef2191931f50a75e868913c93d5b
Tayfun <tayfuna766@gmail.com>
2026-09-08 23:13:53 +0300

    feat(r7): redesign 403 system page

--- views/errors/403.php
+++ views/errors/403.php
@@ -17,8 +17,9 @@ $message = $message ?? 'Bu sayfayı görüntüleme yetkiniz yok.';
 <title>Yetkisiz erişim</title>
 <link rel="icon" href="/assets/img/favicon-32.png" sizes="32x32">
 <link rel="stylesheet" href="<?= Security::e(asset('css/site.css')) ?>">
+<link rel="stylesheet" href="<?= Security::e(asset('css/system-r7.css')) ?>">
 </head>
-<body>
+<body class="system-r7 system-r7--error">
 <main class="system">
   <div class="system__card">
     <p class="system__code">403</p>
