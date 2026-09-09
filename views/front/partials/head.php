1aff109bd7cae165b48091a18e0696ad4edfbfaa
Tayfun <tayfuna766@gmail.com>
2026-09-09 20:28:40 +0300

    feat(redesign): load shared CTA and status styles

--- views/front/partials/head.php
+++ views/front/partials/head.php
@@ -63,6 +63,7 @@ if ($ogImage === '') {
 <link rel="stylesheet" href="<?= Security::e(asset('css/redesign.css')) ?>">
 <link rel="stylesheet" href="<?= Security::e(asset('css/inner-redesign.css')) ?>">
 <link rel="stylesheet" href="<?= Security::e(asset('css/inner-performance.css')) ?>">
+<link rel="stylesheet" href="<?= Security::e(asset('css/r3-cta-status.css')) ?>">
 <?php foreach ((array) ($head['styles'] ?? []) as $style): ?>
 <link rel="stylesheet" href="<?= Security::e(asset((string) $style)) ?>">
 <?php endforeach; ?>
